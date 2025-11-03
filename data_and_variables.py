83ec0a846e5368a38e06b00fda68e8b3142ed137
Madhuri <madhuri.pc30@gmail.com>
2025-11-03 10:25:38 -0600

    saving python

---
+++ data_and_variables.py
@@ -0,0 +1,41 @@
+print(type(5))
+print(type(5.0))
+print(type("5"))
+
+
+print("Data" + "Science")
+print(5 + 5)
+salary = 80000
+print("Salary:" + str(salary))
+
+year= 2025
+month= 10
+print("Date:" + str(year) + "-" +str(month))
+print("Date:" ,year , "-" , month)
+
+print(f"Date:{year} - {month}")
+
+revenue = 13500
+expenses = 8000
+profit = revenue - expenses
+
+print(f"Banana stand profit : ${profit}")
+
+a = 2
+b = 3
+result = a ** b + (10 - 4)/ 2
+print("Results:",round(result))
+#PEDMAS
+
+print(10 % 3)
+
+print(8% 2)
+
+day_number = 23
+print(day_number % 7)
+
+sales = 48000
+costs = 31500
+profit = sales - costs
+profit_margin = (profit /sales ) * 100
+print(f"profit margin :{profit_margin}%")
