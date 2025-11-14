f86055a1fbef81038344c492ee04944dd8e09b7f
Madhuri <madhuri.pc30@gmail.com>
2025-11-13 20:12:55 -0600

    functions-studio

---
+++ collections/exercises/test.py
@@ -0,0 +1,9 @@
+comics = {
+    'Georgia Dunn' : 'Breaking Cat News',
+    'Jan Eliot' : 'Stone Soup',
+    'Wiley Miller' : 'Non Sequitur',         
+    'Bill Watterson' : 'Calvin and Hobbs'
+}
+
+for key in comics.keys():
+    print(key, comics[key])
